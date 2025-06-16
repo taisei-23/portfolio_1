4cb44c16613e9fbd747253cfb61cfb340785f05a
root <root@szktisi13.localdomain>
2025-06-17 00:52:49 +0900

    post作成機能とサイドバーUI改良

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+
   has_one_attached :icon_image
+  has_many :posts, dependent: :destroy
 end
