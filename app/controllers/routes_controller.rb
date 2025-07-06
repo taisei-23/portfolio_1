ec11afabbe8c94dc0d597b61a793478a99b26268
root <root@szktisi13.localdomain>
2025-07-06 23:45:11 +0900

    経路検索にて複数経路を表示可能に

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -17,30 +17,31 @@ class RoutesController < ApplicationController
   end
 
   def fetch_directions(from, to, transport)
-  api_key = ENV["GOOGLE_MAPS_API_KEY"]
-  mode = "driving"
-  base_url = "https://maps.googleapis.com/maps/api/directions/json"
+    api_key = ENV["GOOGLE_MAPS_API_KEY"]
+    mode = "driving"
+    base_url = "https://maps.googleapis.com/maps/api/directions/json"
 
-  uri = URI.parse("#{base_url}?origin=#{URI.encode_www_form_component(from)}&destination=#{URI.encode_www_form_component(to)}&mode=#{mode}&key=#{api_key}")
+    uri = URI.parse("#{base_url}?origin=#{URI.encode_www_form_component(from)}&destination=#{URI.encode_www_form_component(to)}&mode=#{mode}&alternatives=true&key=#{api_key}")
 
-  response = Net::HTTP.get_response(uri)
-  json = JSON.parse(response.body)
+    response = Net::HTTP.get_response(uri)
+    json = JSON.parse(response.body)
 
-  Rails.logger.info "Directions API request URL: #{uri}"
-  Rails.logger.info "Directions API response: #{json}"
+    Rails.logger.info "Directions API request URL: #{uri}"
+    Rails.logger.info "Directions API response: #{json}"
 
-  if json["status"] == "OK"
-    leg = json["routes"][0]["legs"][0]
-    {
-      distance: leg["distance"]["text"],
-      duration: leg["duration"]["text"],
-      start_address: leg["start_address"],
-      end_address: leg["end_address"],
-      steps: leg["steps"].map { |step| step["html_instructions"] }
-    }
-  else
-    Rails.logger.error("Directions API error: #{json['status']} - #{json['error_message']}")
-    nil
+    if json["status"] == "OK"
+      return json["routes"].map do |route|
+        leg = route["legs"][0]
+        {
+          distance: leg["distance"]["text"],
+          duration: leg["duration"]["text"],
+          start_address: leg["start_address"],
+          end_address: leg["end_address"]
+        }
+      end
+    else
+      Rails.logger.error("Directions API error: #{json['status']} - #{json['error_message']}")
+      return nil
+    end
   end
-end
 end
