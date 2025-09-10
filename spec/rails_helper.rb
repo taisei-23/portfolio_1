d63aaf248ce3b758a168d89100f3d3b3a4ad7f40
root <root@szktisi13.localdomain>
2025-09-10 23:05:52 +0900

    修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV['RAILS_ENV'] ||= 'test'
+ENV['RAILS_ENV'] = 'test'
 
 # 初期環境はRAILS_ENV=developmentなので、テストはRAILS_ENV=test bundle exec rspecで実行すること
 
