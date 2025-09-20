312213ef0a2d56bc9c8f762dfa9cf242af658244
root <root@szktisi13.localdomain>
2025-09-20 16:53:20 +0900

    idをuuidに変更

--- db/migrate/20250605144513_create_users.rb
+++ db/migrate/20250605144513_create_users.rb
@@ -1,10 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
-    create_table :users do |t|
-      t.string :name, null: false
-      t.string :email, null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-
+    create_table :users, id: :uuid do |t|
+      t.string :name
+      t.string :email
       t.timestamps
     end
 
